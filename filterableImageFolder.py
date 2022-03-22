f0adfe9f25b683d75a1daea09f9daa507106a8dc
SDFDSJFJ <54651776+SDFDSJFJ@users.noreply.github.com>
2022-03-22 22:28:41 +0800

    Add files via upload

---
+++ filterableImageFolder.py
@@ -0,0 +1,30 @@
+from torchvision.datasets.folder import *
+from typing import *
+
+
+class FilterableImageFolder(ImageFolder):
+    def __init__(
+            self,
+            root: str,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            loader: Callable[[str], Any] = default_loader,
+            is_valid_file: Optional[Callable[[str], bool]] = None,
+            valid_classes: List = None
+    ):
+        self.valid_classes = valid_classes
+        super(FilterableImageFolder, self).__init__(root, transform, target_transform, loader, is_valid_file)
+
+    def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
+        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+        classes = [valid_class for valid_class in classes if valid_class in self.valid_classes]
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
+
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+
+dataset = FilterableImageFolder(r'D:\Data\temp\tai\train', valid_classes=['1000'])
+print(dataset)
+print(dataset.classes)
